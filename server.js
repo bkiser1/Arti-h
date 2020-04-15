611edcf59ec63c44d9ca6831bf85e2ad426085af
brian K <briank@brians-MacBook-Air.local>
2020-04-15 16:55:26 -0400

    made changes to index.html

--- server.js
+++ server.js
@@ -9,21 +9,10 @@ connectDb();
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
-
 ///public Route Defined
-const publicRoute = path.join(__dirname, "/public");
-const viewsPath = path.join(__dirname, "/template/views");
-
-app.set("views engine", "hbs");
-
-app.get("/", (req, res) => {
-	res.render(index);
-});
+const publicRoute = path.join(__dirname, "./public");
 
 //static pages
 app.use(express.static(publicRoute));
 
-app.get("/", (req, res) => {
-	res.render("index");
-});
+app.listen(port, () => console.log(`Server running on port ${port}`));
