c87d3a7a84cfccc960a906b7f90710e363aed026
brian K <briank@brians-MacBook-Air.local>
2020-04-15 13:39:19 -0400

    initial commit

---
+++ public/js/index.js
@@ -0,0 +1,1 @@
+console.log("ollly oxin free");
---
+++ server.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const express = require("express");
+const connectDb = require("./config/db");
+
+const app = express();
+
+//call db
+connectDb();
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
+
+///public Route Defined
+const publicRoute = path.join(__dirname, "/public");
+
+//static pages
+app.use(express.static(publicRoute));
+
+app.get("/", (req, res) => {
+	res.render("index");
+});
